2624722291dbe55bd3cbc5262d5d6bb92f156a5d
Carlos Daniel Londoño Avila <carlosdaniel.londono@utp.edu.co>
2021-02-08 10:36:44 -0500

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -9,30 +9,31 @@ using namespace std;
 
 template <typename T>
 
-class Queue{
-		//Attributes
-	private:
-		list<T> storage;
-	public:
-		// Constructor
-		Queue(): storage() {}
+class Queue {
+  // Attributes
+ private:
+  list<T> storage;
 
-		// Destructor
-        ~Queue() = default;
-		
-		// Return size Queue
-		unsigned int size() const{ return storage.size(); }
-		
-		// Insert an element into the Queue
-		void push(const T& e){ storage.push_back(e); }
-		
-		// Remove an element from the Queue
-		void pop(){ storage.pop_front(); }
-	
-		// Return front Queue
-		T front() { return storage.front(); }
-		
-		// Evaluate if the Queue is empty
-		bool empty() const{ return storage.empty(); }
+ public:
+  // Constructor
+  Queue() : storage() {}
+
+  // Destructor
+  ~Queue() = default;
+
+  // Return size Queue
+  unsigned int size() const { return storage.size(); }
+
+  // Insert an element into the Queue
+  void push(const T& e) { storage.push_back(e); }
+
+  // Remove an element from the Queue
+  void pop() { storage.pop_front(); }
+
+  // Return front Queue
+  T front() { return storage.front(); }
+
+  // Evaluate if the Queue is empty
+  bool empty() const { return storage.empty(); }
 };
 #endif
